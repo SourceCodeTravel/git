6a2d615427417ffdfe9ad5fad43c67f6c72cdebc
David <david@ck.com>
2017-11-08 16:51:44 +0800

    2017年 11月 08日 星期三 16:51:33 CST

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
-PROG=init-db
+PROG=init-db write-tree
+
+all: $(PROG)
 
 init-db: init-db.o
+
+write-tree: write-tree.o
 
 clean:
 	rm -f *.o $(PROG)
--- cache.h
+++ cache.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <string.h>//
 
 #define DEFAULT_DB_ENVIRONMENT ".dircache/objects"
 
